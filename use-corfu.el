082121a8116e0a16f52a7d5176ecbec728d447c9
SBZORRO <sbzorro@gmail.com>
2025-01-20 17:33:31 +0800

    mark sexp

--- use-corfu.el
+++ use-corfu.el
@@ -22,17 +22,17 @@
   (global-corfu-mode))
 
 ;; Use Dabbrev with Corfu!
-(use-package dabbrev
-  :ensure t
-  ;; Swap M-/ and C-M-/
-  :bind (("M-/" . dabbrev-completion)
-          ("C-M-/" . dabbrev-expand))
-  :config
-  (add-to-list 'dabbrev-ignored-buffer-regexps "\\` ")
-  ;; Since 29.1, use `dabbrev-ignored-buffer-regexps' on older.
-  (add-to-list 'dabbrev-ignored-buffer-modes 'doc-view-mode)
-  (add-to-list 'dabbrev-ignored-buffer-modes 'pdf-view-mode)
-  (add-to-list 'dabbrev-ignored-buffer-modes 'tags-table-mode))
+;; (use-package dabbrev
+;;   :ensure t
+;;   ;; Swap M-/ and C-M-/
+;;   :bind (("M-/" . dabbrev-completion)
+;;           ("C-M-/" . dabbrev-expand))
+;;   :config
+;;   (add-to-list 'dabbrev-ignored-buffer-regexps "\\` ")
+;;   ;; Since 29.1, use `dabbrev-ignored-buffer-regexps' on older.
+;;   (add-to-list 'dabbrev-ignored-buffer-modes 'doc-view-mode)
+;;   (add-to-list 'dabbrev-ignored-buffer-modes 'pdf-view-mode)
+;;   (add-to-list 'dabbrev-ignored-buffer-modes 'tags-table-mode))
 
 ;; Optionally use the `orderless' completion style.
 (use-package orderless
