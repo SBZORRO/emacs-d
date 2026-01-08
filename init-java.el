0209e8975208ed2c8726e5e0d3fa67a779f048af
SBZORRO <sbzorro@gmail.com>
2026-01-08 10:01:05 +0800

    update remote gnuplot

--- init-java.el
+++ init-java.el
@@ -17,16 +17,6 @@
 
   (add-to-list 'eglot-server-programs `(java-mode . ,#'my/eglot-jdtls-contact)))
 
-;; ---- 补全 (Corfu) ----
-(use-package corfu
-  :init
-  (global-corfu-mode))
-
-;; ---- 语法高亮/语义增强 ----
-(use-package treesit
-  :when (treesit-available-p)
-  :config
-  (add-to-list 'major-mode-remap-alist '(java-mode . java-ts-mode)))
 
 ;; ---- 格式化保存 ----
 (defun my/java-eglot-format-on-save ()
