d7cfdee1d2e058f84b98c5f6fa106402c2167612
SBZORRO <sbzorro@gmail.com>
2026-01-08 15:08:34 +0800

    orderless

--- use-package.el
+++ use-package.el
@@ -7,10 +7,13 @@
 (use-package orderless
   :load-path "orderless"
   :custom
-  ;; (orderless-style-dispatchers '(orderless-affix-dispatch))
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch))
   ;; (orderless-component-separator #'orderless-escapable-split-on-space)
-  (completion-styles '(orderless))
-  (completion-category-overrides '((file (styles basic partial-completion)))))
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles partial-completion))))
+  (completion-category-defaults nil) ;; Disable defaults, use our settings
+  (completion-pcm-leading-wildcard t)) ;; Emacs 31: partial-completion behaves like substring
 
 (use-package yasnippet
   :load-path "yasnippet"
